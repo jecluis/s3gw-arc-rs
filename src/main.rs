5cc0ef1815ceee4acb9687d18b5140a8a9fa58ca
Joao Eduardo Luis <joao@suse.com>
2023-09-11 06:26:45 +0000

    arc: disable logging by default
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ mod ws;
 #[tokio::main]
 async fn main() {
     env_logger::builder()
-        // .filter_level(log::LevelFilter::Info)
+        .filter_level(log::LevelFilter::Off)
         .parse_env("ARC_DEBUG")
         .try_init()
         .unwrap();
